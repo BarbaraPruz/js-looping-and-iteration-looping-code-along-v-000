40d035bceeeea6b97b86624a2fbc49e1116d5a86
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-04 22:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solutions in this file
+function printBadges(names) {
+  for (let i = 1; i <= names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i}.`);
+ }
+}
+// LOG: Welcome Ada! You are employee #1.
